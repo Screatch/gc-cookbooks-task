c6bf7009c7cebc3750d5b72f9bc84dde4fb4e2ae
Vitali Raikov <screatch@gmail.com>
2017-09-30 23:04:40 +0300

    experimenting with web.json

--- solo.rb
+++ solo.rb
@@ -1,7 +1,6 @@
 root = File.absolute_path(File.dirname(__FILE__))
 
 file_cache_path root
-
 cookbook_path root + '/cookbooks'
 role_path root + '/cookbooks/roles'
 json_attribs root+ "/cookbooks/web.json"
